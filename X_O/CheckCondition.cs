540b6fecc3ada162d313121b4446db9da0798446
zuievs <zuievsye@gmail.com>
2023-04-27 13:04:59 -0400

    Add project files.

---
+++ X_O/CheckCondition.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace X_O
+{
+    public class CheckCondition
+    {
+        public void DisableButton(Button[,] board)
+        {
+            foreach (Button button in board)
+            {
+                button.IsEnabled = false;
+            }
+        }
+        public void ResetGame(Button[,] board)
+        {
+            foreach (Button button in board) 
+            {
+                button.IsEnabled = true;
+                button.Content = "";
+            }
+        }
+        public bool IsBoardFull(Button[,] board)
+        {
+            foreach (Button button in board)
+            {
+                if (button.Content == null || button.Content.ToString() == "")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckWin(Button[,] board, string player)
+        {
+            // Проверяем по горизонтали
+            for (int i = 0; i< 3; i++)
+            {
+                if (board[i, 0].Content == player &&
+                    board[i, 1].Content == player &&
+                    board[i, 2].Content == player)
+                {
+                    return true;
+                }
+            }
+
+            // Проверяем по вертикали
+            for (int i = 0; i< 3; i++)
+            {
+                if (board[0, i].Content == player &&
+                    board[1, i].Content == player &&
+                    board[2, i].Content == player)
+                {
+                    return true;
+                }
+            }
+
+            // Проверяем по диагонали сверху-вниз
+            if (board[0, 0].Content == player &&
+                board[1, 1].Content == player &&
+                board[2, 2].Content == player)
+{
+    return true;
+}
+
+// Проверяем по диагонали снизу-вверх
+if (board[0, 2].Content == player &&
+    board[1, 1].Content == player &&
+    board[2, 0].Content == player)
+{
+    return true;
+}
+
+return false;
+        }
+    }
+}
---
+++ X_O/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace X_O
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string originalButtonText;
+        bool xoCheck = true;
+        CheckCondition checkCondition = new CheckCondition();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button[,] buttons = new Button[,]
+            {
+                    {Button1, Button2, Button3},
+                    {Button4, Button5, Button6},
+                    {Button7, Button8, Button9}
+            };
+            Button clickedButton = sender as Button;
+            if (xoCheck)
+            {
+                clickedButton.Content = "X";
+            }
+            else
+            {
+                clickedButton.Content = "O";
+            }
+            xoCheck = !xoCheck;
+            if (checkCondition.IsBoardFull(buttons))
+            {
+                MessageBox.Show("Перемогла Дружба");
+                checkCondition.DisableButton(buttons);
+            }
+            else
+            { 
+                if (checkCondition.CheckWin(buttons, "X"))
+                {
+                    MessageBox.Show("Перемогли Хрестики");
+                    foreach (Button button in buttons)
+                    checkCondition.DisableButton(buttons);
+                }
+                if (checkCondition.CheckWin(buttons, "O"))
+                    {
+                    MessageBox.Show("Перемогли Нулики");
+                    checkCondition.DisableButton(buttons);
+                }
+            }
+
+        }
+
+        private void Button_Click_NewGame(object sender, RoutedEventArgs e)
+        {
+            Button[,] buttons = new Button[,]
+            {
+                    {Button1, Button2, Button3},
+                    {Button4, Button5, Button6},
+                    {Button7, Button8, Button9}
+            };
+            xoCheck = true;
+            checkCondition.ResetGame(buttons);
+        }
+
+    }
+}
